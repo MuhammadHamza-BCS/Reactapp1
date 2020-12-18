c1f3b4f9d6535a7141c3b8e121a7a601be01e4b8
MuhammadHamzaAttari <hamzaattariqadriziaee@gmail.com>
2020-12-19 03:32:01 +0500

    React components

---
+++ src/List.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function List(){
+    return( <ul>
+        <li>Hamza</li>
+        <li>Sajid</li>
+        <li>Attari</li>
+        <li>Qadri</li>
+    </ul>);
+}
+export default List;
